1a045e74a719c7f1bbbead6fafcaf406d1e3df5e
Mohammad Maaz <76915918+md-maaz@users.noreply.github.com>
2022-04-29 11:12:29 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title('My Partners New Healthy Diner')
 st.header('Breakfast Menu')
-at.text('Peanut butter & Jelly')
+st.text('Peanut butter & Jelly')
 st.text('Choco Pie')
 st.text('Biryani')
