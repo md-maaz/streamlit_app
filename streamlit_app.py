1f26d542620ea379f0b44e522463f2c64d73736c
Mohammad Maaz <76915918+md-maaz@users.noreply.github.com>
2022-04-29 15:05:38 +0530

    Update streamlit_app.py
    filter our table data .

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,11 @@ import pandas as pd
 data_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 data_list = data_list.set_index('Fruit')
 
-# adding multiselect 
-streamlit.multiselect('Pick some fruits', list(data_list.index),['Avocado','Strawberries'])
+# adding multiselect
+# picking the variables only to show in select 
+fruits_selected = streamlit.multiselect('Pick some fruits', list(data_list.index),['Avocado','Strawberries'])
+# fruits to show 
+fruits_to _show=data_list.loc[fruits_selected]
 
 streamlit.dataframe(data_list)
 
