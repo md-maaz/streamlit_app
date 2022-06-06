57f525976b8cead925325f1739c5000ac54bb2be
Mohammad Maaz <76915918+md-maaz@users.noreply.github.com>
2022-06-06 23:41:39 +0530

    Update price_pred_app.py
    Import libraries

--- price_pred_app.py
+++ price_pred_app.py
@@ -1,1 +1,10 @@
+pip install fbprophet yfinance plotly 
+
+import streamlit as st 
+from datetime import date 
+
+import yfinance as yf 
+from fbprophet import Prophet 
+from fbprophet import plot_plotly 
+from plotly import graph_objs as go 
 
