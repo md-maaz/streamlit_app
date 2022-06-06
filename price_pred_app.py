d47254ec98fb42306867be77fbe757cefd99f916
Mohammad Maaz <76915918+md-maaz@users.noreply.github.com>
2022-06-06 23:45:06 +0530

    Update price_pred_app.py
    Added date, stocks option & select box

--- price_pred_app.py
+++ price_pred_app.py
@@ -8,3 +8,10 @@ from fbprophet import Prophet
 from fbprophet import plot_plotly 
 from plotly import graph_objs as go 
 
+START = '2015-01-01'
+TODAY = date.today().strftime('%Y-%m-%d')
+
+st.title('STOCKS FORECAST APP')
+
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+select_stock = st.selectbox('Select Stock for prediction',stocks)
